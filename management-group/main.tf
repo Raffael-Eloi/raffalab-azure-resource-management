99afad1ba1cd445f2b826ebc536ed68524d75271
Raffael Eloi <raffaeleloi121@gmail.com>
2026-07-30 20:12:57 -0300

    refactor management-group terraform file

--- management-group/main.tf
+++ management-group/main.tf
@@ -2,13 +2,10 @@ data "azurerm_subscription" "current" {}
 
 resource "azurerm_management_group" "main" {
   display_name = "RaffaLabManagementGroup"
-}
-
-data "azurerm_subscription" "main" {
-  subscription_id = data.azurerm_subscription.current.subscription_id
+  name         = "mg-raffalab"
 }
 
 resource "azurerm_management_group_subscription_association" "main" {
   management_group_id = azurerm_management_group.main.id
-  subscription_id     = data.azurerm_subscription.main.id
+  subscription_id     = data.azurerm_subscription.current.id
 }
